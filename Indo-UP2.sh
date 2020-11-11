5210bb5e3f7e627a5002b2317fe7fd285402d9ba
Cybernet5 <74086917+Cybernet5@users.noreply.github.com>
2020-11-12 02:18:05 +0700

    Update Version 0.2
    git clone https://github.com/cybernet5/Indo-Tool2
    cd Indo-Tool2
    sh Indo-UP2.sh
    
    Tools akan di update setiap Bulan

---
+++ Indo-UP2.sh
@@ -0,0 +1,341 @@
+#!/system/xbin/bash
+
+#====================================================
+#Record lah Sepuas Kalian
+#Karna saya ga mau,, ilmu yang saya miliki
+#Berhenti di diri saya sendiri,, itu Sangat Sia Sia :)
+#Save No admin [ 089514334669 ]
+
+#Version : 0.2
+#====================================================
+
+clear
+xdg-open https://chat.whatsapp.com/J9LmAY9LaqOLnTBo4ZRTSN
+
+echo "\033[31;1m██╗███╗   ██╗██████╗  ██████╗       ██╗   ██╗██████╗" 
+echo "\033[31;1m██║████╗  ██║██╔══██╗██╔═══██╗      ██║   ██║██╔══██╗"
+echo "\033[31;1m██║██╔██╗ ██║██║  ██║██║   ██║█████╗██║   ██║██████╔╝"
+echo "\033[37;1m██║██║╚██╗██║██║  ██║██║   ██║╚════╝██║   ██║██╔═══╝" 
+echo "\033[37;1m██║██║ ╚████║██████╔╝╚██████╔╝      ╚██████╔╝██║"     
+echo "\033[37;1m╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝        ╚═════╝ ╚═╝"
+echo "\033[33;1m○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○"
+echo "\033[35;1m ● \033[37;1mAuthor  \033[36;1m| \033[30;1mMr.IngsS"
+echo "\033[36;1m ● \033[37;1mRegion  \033[36;1m| \033[31;1mIndon\033[37;1mesian"
+echo "\033[32;1m ● \033[37;1mGithub  \033[36;1m| \033[30;1mhttps://github.com/cybernet5"
+echo "\033[34;1m ● \033[37;1mContact \033[36;1m| \033[30;1mcybernet5@mbox.re"
+echo "\033[34;1m ● \033[37;1mProgram \033[36;1m| \033[30;1mBash/Shell"
+echo "\033[33;1m○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○ \033[37;1m[ \033[35;1mV.02\033[37;1m]"
+echo ""
+echo "\033[36;1m ● \033[41;1m\033[37;1mINSTALLING BAHAN TERLEBIH DAHULU\033[00;1m \033[37;1m[ \033[32;1m● \033[36;1m2020 \033[37;1m]"
+echo "\033[00;1m"
+echo "\033[31;1m[ \033[37;1m01 \033[31;1m] \033[33;1m● \033[32;1mBruteforce Facebook"
+echo "\033[31;1m[ \033[37;1m02 \033[31;1m] \033[33;1m● \033[32;1mAdmin-Finder"
+echo "\033[31;1m[ \033[37;1m03 \033[31;1m] \033[33;1m● \033[32;1mKnifer Tools"
+echo "\033[31;1m[ \033[37;1m04 \033[31;1m] \033[33;1m● \033[32;1mFsociety"
+echo "\033[31;1m[ \033[37;1m05 \033[31;1m] \033[33;1m● \033[32;1mSaydog-Framework"
+echo "\033[31;1m[ \033[37;1m06 \033[31;1m] \033[33;1m● \033[32;1mSpoofing Email"
+echo "\033[31;1m[ \033[37;1m07 \033[31;1m] \033[33;1m● \033[32;1mXAttacker Tools Premium"
+echo "\033[31;1m[ \033[37;1m08 \033[31;1m] \033[33;1m● \033[32;1mVuln-X"
+echo "\033[31;1m[ \033[37;1m09 \033[31;1m] \033[33;1m● \033[32;1mCreate Virus Mallware"
+echo "\033[31;1m[ \033[37;1m10 \033[31;1m] \033[33;1m● \033[32;1mTh3Inspector-Tools"
+echo "\033[31;1m[ \033[37;1m11 \033[31;1m] \033[33;1m● \033[32;1mHacker Pro Tools"
+echo "\033[31;1m[ \033[37;1m12 \033[31;1m] \033[33;1m● \033[32;1mTools AnonHacker"
+echo "\033[31;1m[ \033[37;1m13 \033[31;1m] \033[33;1m● \033[41;1m\033[37;1mInstalling Bahan\033[00;1m"
+echo "\033[31;1m[ \033[37;1m14 \033[31;1m] \033[33;1m● \033[32;1mLazymux-Tools"
+echo "\033[31;1m[ \033[37;1m15 \033[31;1m] \033[33;1m● \033[32;1mPishing Spoofing Mailer"
+echo "\033[31;1m[ \033[37;1m16 \033[31;1m] \033[33;1m● \033[32;1mWSLTools"
+echo "\033[31;1m[ \033[37;1m17 \033[31;1m] \033[33;1m● \033[32;1mSetToolkit Installer"
+echo "\033[31;1m[ \033[37;1m18 \033[31;1m] \033[33;1m● \033[32;1mFacebook Cracker\033[33;0m\033[41;1m\033[37;1mLogin Token\033[33;0m"
+echo "\033[31;1m[ \033[37;1m19 \033[31;1m] \033[33;1m● \033[32;1mCloning Email"
+echo "\033[31;1m[ \033[37;1m20 \033[31;1m] \033[33;1m● \033[32;1mExiting Program"
+echo "\033[37;1m"
+read -p "Input Number : " pilih
+if [ $pilih = "01" ]
+then
+    clear
+    echo "\033[32;1mInstalling Bruteforce Facebook \033[37;1m..."
+    echo ""
+    sleep 2
+    cd $HOME
+    git clone https://github.com/HostIngsS/IngsBF.git
+    cd IngsBF
+    nano pass.txt
+    python2 Bruteings.py
+fi
+
+if [ $pilih = "02" ]
+then
+    clear
+    echo "\033[32;1mInstalling Admin-Finder \033[37;1m..."
+    echo ""
+    sleep 2
+    xdg-open https://youtu.be/liOJmXxsWl0
+    cd $HOME
+    git clone https://github.com/s0md3v/Breacher
+    cd Breacher
+    pip install -r paths.txt
+    echo "\033[32;1m● \033[37;1mKetik : \033[32;1mpython2 breacher.py -u \033[37;1m[ Website- Target ]"
+fi
+
+if [ $pilih = "03" ]
+then
+    clear
+    echo "\033[32;1mInstalling Knifer Tools \033[37;1m..."
+    echo ""
+    sleep 2
+    xdg-open https://youtu.be/8D3f-w1MwUM
+    cd $HOME
+    xdg-open https://youtu.be/8D3f-w1MwUM
+    git clone https://github.com/BladeKnife/knf-tool
+    cd knf-tool
+    sh install.sh
+fi
+
+if [ $pilih = "04" ]
+then
+    clear
+    echo "\033[32;1mInstalling Fsociety \033[37;1m..."
+    echo ""
+    sleep 2
+    xdg-open https://youtu.be/p23wxZm4WVU
+    cd $HOME
+    git clone https://github.com/Manisso/fsociety
+    cd fsociety
+    chmod +x *
+    ./fsociety.py
+fi
+
+if [ $pilih = "05" ]
+then
+    clear
+    echo "\033[32;1mInstalling Saydog-Framework \033[37;1m..."
+    echo ""
+    sleep 2
+    xdg-open https://instagram.com/saydog.official?igshid=6mxrawk2w8yn
+    cd $HOME
+    git clone https://github.com/saydog-official/saydog-framework.git
+    cd saydog-framework
+    chmod +x *
+    pip install -r requirements.txt
+    bash setup
+fi
+
+if [ $pilih = "06" ]
+then
+    clear
+    echo "\033[32;1mInstalling Spoofing Email \033[37;1m..."
+    echo ""
+    sleep 2
+    cd $HOME
+    git clone https://github.com/ByDog3r/Faque
+    cd Faque
+    chmod +x faque.sh
+    bash faque.sh
+fi
+
+if [ $pilih = "07" ]
+then
+     clear
+    echo "\033[32;1mInstalling XAttacker Tools Premium \033[37;1m..."
+    echo ""
+    sleep 2
+    cd $HOME
+    git clone https://github.com/Moham3dRiahi/XAttacker.git
+    cd XAttacker
+    chmod +x termux-install.sh
+    bash termux-install.sh
+fi
+
+if [ $pilih = "08" ]
+then
+    clear
+    echo "\033[32;1mInstalling Vuln-X\033[37;1m..."
+    echo ""
+    sleep 2
+    cd $HOME
+    git clone http://github.com/anouarbensaad/vulnx
+    cd vulnx
+    chmod +x install.sh
+    ./install.sh
+fi
+
+if [ $pilih = "09" ]
+then
+    clear
+    echo "\033[32;1mInstalling Create Virus Mallware \033[37;1m..."
+    echo ""
+    sleep 2
+    cd $HOME
+    git clone https://github.com/BlackCyberAnonim/B-VIRUS.git
+    cd B-VIRUS
+    python2 B-VIRUS.py
+fi
+
+if [ $pilih = "10" ]
+then
+    clear
+    echo "\033[32;1mInstalling Th3Inspector-Tools\033[37;1m..."
+    echo ""
+    sleep 2
+    cd $HOME
+    git clone https://github.com/Moham3dRiahi/Th3inspector.git
+    cd Th3inspector
+    chmod +x install.sh
+    ./install.sh
+fi
+
+if [ $pilih = "11" ]
+then
+    clear
+    echo "\033[32;1mInstalling Hacker Pro Tools \033[37;1m..."
+    echo ""
+    sleep 2
+    cd $HOME
+    git clone https://github.com/technicaldada/hackerpro.git
+    cd hackerpro
+    python2 hackerpro.py
+fi
+
+if [ $pilih = "12" ]
+then
+    clear
+    echo "\033[32;1mInstalling Tools AnonHacker\033[37;1m..."
+    echo ""
+    sleep 2
+    cd  $HOME
+    git clone https://github.com/AnonHackerr/toolss.git
+    cd toolss
+    chmod +x Tools.py
+    python Tools.py
+fi
+
+if [ $pilih = "13" ]
+then
+    clear
+    echo "\033[32;1mInstalling Installing Bahan \033[37;1m..."
+    sleep 1
+    echo "\033[32;1m● \033[37;1mMohon Tunggu \033[37;1m..."
+    echo ""
+    sleep 3
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install python -y
+    apt install python3
+    pkg install python3
+    pkg install nmap -y
+    pkg install python2 -y
+    pkg install fish
+    pkg install nano
+    pkg install wget
+    pkg install mc
+    pkg install curl
+    pkg install git
+    pkg install libxslt
+    pkg install clang
+    pkg install php
+    pip install Cython
+    pip2 install requests
+    pip2 install tqdm
+    pip install tqdm
+    pip2 install yagmail
+    pip install yagmail
+    pip install requests
+    pip install --upgrade pip
+    pip2 install mechanize
+    pip install mechanize
+    pip2 install bs4
+    pip install bs4
+    pip2 install colorama
+    pip install colorama
+    python3 -m pip install bs4
+    python3 -m pip install requests
+    echo "\033[32;1m● \033[37;1mInstalling Bahan Success"
+    sleep 10
+    echo ""
+    echo "\033[32;1m● \033[31;1mKetikan \033[30;1m: \033[37;1msh Indo-UP2.sh"
+fi
+
+if [ $pilih = "14" ]
+then
+    clear
+    echo "\033[32;1mInstalling Lazymux \033[37;1m..."
+    echo ""
+    sleep 2
+    cd $HOME
+    git clone https://github.com/Gameye98/Lazymux
+    cd Lazymux
+    chmod +x lazymux.py
+    python lazymux.py
+fi
+
+if [ $pilih = "15" ]
+then
+    clear
+    echo "\033[32;1mInstalling Pishing Spoofing Mailer \033[37;1m..."
+    echo ""
+    sleep 2
+    cd $HOME 
+    git clone git clone https://www.github.com/BiZken/PhishMailer.git
+    cd PhishMailer
+    chmod +x PhishMailer.py
+    python3 PhishMailer.py
+fi
+
+if [ $pilih = "16" ]
+then
+    clear
+    echo '"\033[32;1mInstalling WSLTools \033[37;1m..."
+    echo ""
+    sleep 2
+    cd $HOME
+    git clone https://github.com/AnonHackerr/WSLTools.git
+    cd WSLTools
+    python WSLTools.py
+fi
+
+if [ $pilih = "17" ]
+then
+    clear
+    echo "\033[32;1mInstalling SetToolkit Installer \033[37;1m..."
+    echo ""
+    sleep 2
+    cd $HOME
+    git clone https://github.com/AnonHackerr/setoolkitinstaller.git
+    cd setoolkitinstaller
+    chmod +x setoolkitinstall.sh
+    ./setoolkitinstall.sh
+fi
+
+if [ $pilih = "18" ]
+then
+    clear
+    echo "\033[32;1mInstalling Facebook Cracker \033[37;1m..."
+    echo ""
+    sleep 2
+    cd $HOME
+    git clone https://github.com/Cybernet5/Fb-Random-Crack.git
+    cd Fb-Random-Crack
+    python FB-Random-Crack.py
+fi
+
+if [ $pilih = "19" ]
+then
+    clear
+    echo "\033[32;1mInstalling Cloning Email \033[37;1m..."
+    echo ""
+    sleep 2
+    cd $HOME
+    git clone https://github.com/Dunia-Kode/Cloning
+    cd Cloning
+    python clone.py
+fi
+
+if [ $pilih = "20" ]
+then
+    clear
+    sleep 3
+    cd $HOME
+    echo "\033[32;1m • \033[37;1mExiting Program \033[31;1m!!!"
+    sleep 2
+    xdg-open https://wa.wizard.id/fa0197
+fi
